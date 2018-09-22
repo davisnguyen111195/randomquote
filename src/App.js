204020156bbb67f66028bef6ba0b76341c328e49
davisnguyen111195 <nguyendat111195@gmail.com>
2018-09-22 16:44:34 +0700

    change author

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Front author="-david Nguyen Oprah Winfrey" content="If you look at what you have in life, you’ll always have more. If you look at what you don’t have in life, you’ll never have enough."/>
+        <Front author="Oprah Winfrey" content="If you look at what you have in life, you’ll always have more. If you look at what you don’t have in life, you’ll never have enough."/>
       </div>
     );
   }
